74c83d3f60e07b1f18ce99e86ce95ca5fc0739fd
ahmad.shahid <ahmad.shahid@hrsdc-rhdcc.gc.ca>
2026-04-16 07:51:42 -0400

    CDTS v5.1.0 Release

---
+++ dist/app/cls/WET/gcweb/v5_1_0/wet-boew/js/gcdsloader.js
@@ -0,0 +1,67 @@
+
+/**
+ * @title GCDS loader
+ * @overview Script to load GCDS which was tested for co-existance in GCWeb
+ * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
+ * @author @duboisp
+ */
+( function( wnd, doc, escape ) {
+"use strict";
+
+
+//
+// Configuration updated during the build process with info in package.json
+//
+const gcdsVersion = "1.0.0", // pkg.peerDependencies[ "@cdssnc/gcds-components" ]
+	gcdsSriJs = "sha384-QHV+zvCrAiITDds95HYvkDrBJOUItYeeo4FNEq/cSxJDIrDbIRnwc3brrTxv8Oi6", // pkg[ "io.github.wet-boew" ].gcdsSriJs
+	gcdsSriCss = "sha384-lRbhe4rCI9asFFK+Bho5e2mck4JXt3I08f6WNGpgZ1mRqT3P11QErcxIeLIRhWOa"; // pkg[ "io.github.wet-boew" ].gcdsSriCss
+
+
+//
+// General configuration used to detect and load GCSD
+//
+const gcdsBasePath = "https://cdn.design-system.canada.ca/@gcds-core/components@",
+	gcdsSuffixJs = "/dist/gcds/gcds.esm.js",
+	gcdsSuffixCss = "/dist/gcds/gcds.css",
+	txtScript = "script",
+	txtLink = "link",
+	blockingState = "render";
+
+
+//
+// Check if GCDS lib are already defined in the header
+//
+let traceFound = doc.querySelector( txtLink + "[href$=" + escape( gcdsSuffixCss ) + "]," + txtScript + "[src$=" + escape( gcdsSuffixJs ) + "]" );
+
+
+//
+// Import the GCDS libs
+//
+if ( !traceFound && gcdsSriJs && gcdsSriCss ) {
+	let scrGcds = doc.createElement( txtScript );
+	scrGcds.blocking = blockingState;
+	scrGcds.type = "module";
+	scrGcds.integrity = gcdsSriJs;
+	scrGcds.src = gcdsBasePath + gcdsVersion + gcdsSuffixJs;
+	doc.head.appendChild( scrGcds );
+
+	let lnkGcds = doc.createElement( txtLink );
+	lnkGcds.blocking = blockingState;
+	lnkGcds.rel = "stylesheet";
+	lnkGcds.crossOrigin = "anonymous";
+	lnkGcds.integrity = gcdsSriCss;
+	lnkGcds.href = gcdsBasePath + gcdsVersion + gcdsSuffixCss;
+	doc.head.appendChild( lnkGcds );
+
+
+	//
+	// Set GCDS version globally once loaded
+	//
+	wnd.addEventListener( "appload", ( event ) => {
+		if ( event.detail.namespace === "gcds" ) {
+			wnd.wbgcds = gcdsVersion;
+		}
+	} );
+}
+
+} )( window, document, CSS.escape );
